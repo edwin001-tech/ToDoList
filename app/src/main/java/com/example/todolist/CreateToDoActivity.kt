e5755f4d1c7b05f8ab320cb4358fd9998bba309e
edwin001-tech <edwinshitakwa8@gmail.com>
2020-05-27 16:06:05 +0300

    made the save button functional

--- app/src/main/java/com/example/todolist/CreateToDoActivity.kt
+++ app/src/main/java/com/example/todolist/CreateToDoActivity.kt
@@ -1,12 +1,31 @@
 package com.example.todolist
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_create_to_do.*
 
 class CreateToDoActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_to_do)
+
+        //create a lambda expression for the save button
+        saveButton.setOnClickListener{
+            var title = ""
+            if(importantCheckBox.isChecked){
+                title = "!" +titleEditText.text.toString()
+            }
+            else{
+                title = titleEditText.text.toString()
+            }
+            var prefs = getSharedPreferences( "com.example.todolist.sharedprefs", Context.MODE_PRIVATE)
+            var todos = prefs.getStringSet( "todostrings", setOf())!!.toMutableSet()
+            todos.add(title)
+
+            prefs.edit().putStringSet( "todostrings",todos).apply()
+            finish()
+        }
     }
 }
