19daf4aa45027b4f561857db720b488caeda0d84
edwin001-tech <edwinshitakwa8@gmail.com>
2020-05-29 11:17:06 +0300

    created todo row.xml

--- app/src/main/java/com/example/todolist/ToDoAdapter.kt
+++ app/src/main/java/com/example/todolist/ToDoAdapter.kt
@@ -1,11 +1,24 @@
 package com.example.todolist
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.todo_row.view.*
 
 class ToDoAdapter (val todos: List<String>) : RecyclerView.Adapter<ToDoAdapter.ToDoHolder>(){
+    override fun onCreateViewHolder(p0: ViewGroup,p1: Int): ToDoHolder {
+        return ToDoHolder(LayoutInflater.from(p0.context).inflate(R.layout.todo_row, p0, false))
+    }
+
+    override fun getItemCount(): Int {
+        return todos.count()
+    }
+    override fun onBindViewHolder(p0: ToDoHolder, p1: Int) {
+        val title =todos[p1]
+        p0.bindTodo(title)
+    }
+
     class ToDoHolder(v:View) :RecyclerView.ViewHolder(v), View.OnClickListener {
         var view : View = v
         var title: String = ""
@@ -22,16 +35,10 @@ class ToDoAdapter (val todos: List<String>) : RecyclerView.Adapter<ToDoAdapter.T
         }
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ToDoHolder {
-        TODO("Not yet implemented")
-    }
 
-    override fun getItemCount(): Int {
-        return todos.count()
-    }
 
-    override fun onBindViewHolder(holder: ToDoHolder, position: Int) {
-        TODO("Not yet implemented")
-    }
+
+
+
 
 }
